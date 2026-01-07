562dbc9996040bd630a7c279a2aa1caf42694d46
nasuda <nasuda908@gmail.com>
2026-01-07 16:56:49 +0900

    add sigint exit

--- first-week/stdin.mjs
+++ first-week/stdin.mjs
@@ -3,8 +3,19 @@ process.stdin.resume();
 
 
 process.stdin.on("data", (chunk) => {
+
+// exit
+  if (chunk.trim() === "exit") {
+    process.stdout.write("ok! exit!\n");
+    process.exit(0);
+  }
   
+//out  
   process.stdout.write(chunk + "\n")
 });
 
-
+//exit
+process.on('SIGINT', () => {
+  process.stdout.write("\nexit\n")
+  process.exit(0);
+});
