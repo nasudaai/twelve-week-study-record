a9abc31a76cdcceabc496fc39ec992eacc8763f1
nasuda <nasuda908@gmail.com>
2026-01-07 16:20:30 +0900

    add comment

--- first-week/main.mjs
+++ first-week/main.mjs
@@ -1,13 +1,15 @@
 //in
 const args = process.argv.slice(2);
-const [key, value] = args;
 
 
 //Handling errors during input
 if (args.length !== 2) {
   process.stdout.write("please arg two!\n");
-  process.exit(1);
+  process.exit(0);
 }
+
+//input process
+const [key, value] = args;
 
 //process or conversion
 
