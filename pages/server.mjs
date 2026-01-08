605459996601c0559b2b3b8c3ccb78bd1840627f
nasuda <nasuda908@gmail.com>
2026-01-08 16:29:19 +0900

    add html

--- pages/server.mjs
+++ pages/server.mjs
@@ -1,4 +1,10 @@
 import { createServer } from 'node:http';
+import { readFileSync } from 'node:fs';
 
 //check spelling on the first line.
 //console.log(createServer);  
+
+
+//read html
+const html = readFileSync("./index.html", "utf-8");
+console.log(html);
