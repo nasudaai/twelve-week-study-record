21287c39a0f9f9522cb81877a15e0dfd9d30ff67
nasuda <nasuda908@gmail.com>
2026-01-09 16:10:52 +0900

    add code

--- first-week/stdin.mjs
+++ first-week/stdin.mjs
@@ -1,21 +1,27 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.resume();
 
+const arr = [];
 
 process.stdin.on("data", (chunk) => {
 
 // exit
   if (chunk.trim() === "exit") {
     process.stdout.write("ok! exit!\n");
+    console.log(arr);
     process.exit(0);
   }
+
+// process
+  arr.push(chunk)
+
   
 //out  
-  process.stdout.write(chunk + "\n")
+  process.stdout.write(chunk);
 });
 
 //exit
 process.on('SIGINT', () => {
-  process.stdout.write("\nexit\n")
+  process.stdout.write("\nexit\n");
   process.exit(0);
 });
