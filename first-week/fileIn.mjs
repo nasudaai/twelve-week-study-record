a99313237716a2fc0637b43530724741b8489c24
nasuda <nasuda908@gmail.com>
2026-01-10 20:52:07 +0900

    read file and write std

--- first-week/fileIn.mjs
+++ first-week/fileIn.mjs
@@ -3,5 +3,7 @@ import { readFileSync } from 'node:fs';
 const args = process.argv.slice(2);
 const file = args[0];
 
+const data = readFileSync(file, "utf-8");
 
 process.stdout.write(file + "\n");
+process.stdout.write(data + "\n");
