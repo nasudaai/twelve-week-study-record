5874290bc7af7f5eae0b20752e7b10caf5ecc234
nasuda <nasuda908@gmail.com>
2026-01-12 08:19:53 +0900

    chunk

--- second-week/main.mjs
+++ second-week/main.mjs
@@ -1,8 +1,15 @@
 const args = process.argv.slice(2);
 
-function lineWrite(args) {
-
+function changeLine(args) {
   const line = args.join(" ");
+  return line;
+}
+
+const line = changeLine(args);
+
+function lineWrite(line) {
+
+//  const line = args.join(" ");
   process.stdout.write(line + "\n");
 }
 
@@ -13,4 +20,4 @@ function write(word) {
 }
 
 write(args[0]);
-lineWrite(args);
+lineWrite(line);
