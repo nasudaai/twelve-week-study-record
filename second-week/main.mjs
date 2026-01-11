6dde3939941c136c63f387405d9c774e3025f574
nasuda <nasuda908@gmail.com>
2026-01-12 07:51:49 +0900

    add "\n"

--- second-week/main.mjs
+++ second-week/main.mjs
@@ -1,7 +1,14 @@
 
+function lineWrite(line) {
+
+  process.stdout.write(line + "\n");
+}
+
+
 function write(word) {
 
-  process.stdout.write("word");
+  process.stdout.write("word" + " ");
 }
 
 write("word");
+lineWrite("a b c");
