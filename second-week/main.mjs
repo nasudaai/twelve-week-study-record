9487100635dbbdaa2618f0b43d97b09e3dab9a14
nasuda <nasuda908@gmail.com>
2026-01-12 20:48:55 +0900

    add branch

--- second-week/main.mjs
+++ second-week/main.mjs
@@ -1,4 +1,12 @@
+import { writeFile } from 'node:fs';
+
+
 const args = process.argv.slice(2);
+if (args.length === 2) {
+  const flag = "w";
+  console.log(flag);
+}
+
 const word = args[0];
 
 function write(word) {
