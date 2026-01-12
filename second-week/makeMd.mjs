6ac8142644e5e260e503f6fc8c0730c9ed7072d9
nasuda <nasuda908@gmail.com>
2026-01-13 08:49:05 +0900

    add error process

--- second-week/makeMd.mjs
+++ second-week/makeMd.mjs
@@ -3,6 +3,12 @@ import { writeFileSync } from 'node:fs';
 
 const args = process.argv.slice(2);
 
+
+if (args.length === 0) {
+  process.stdout.write("usage: <title> or <title> <filename>\n");
+  process.exit(0);
+}
+
 const title = args[0];
 
 writeFileSync("out/" + title + ".md", title + "\n");
