089ca335ff332fe5618f54f201daaa09ba4c7c70
nasuda <nasuda908@gmail.com>
2026-01-13 09:30:57 +0900

    add server and html js

---
+++ second-week/page/server.mjs
@@ -0,0 +1,20 @@
+import { createServer } from 'node:http';
+import { readFileSync } from 'node:fs';
+console.log(createServer);
+
+
+const html = readFileSync("index.html");
+const js = readFileSync("main.js");
+
+const server = createServer((req, res) => {
+  if (req.url === "/main.js") {
+    res.writeHead(200, { "Content-Type": "text/javascript; charset=utf-8" });
+    res.end(js);
+    return;
+  }
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000")
+});
