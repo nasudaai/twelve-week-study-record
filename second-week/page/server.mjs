1707ba1aa2f1df9f485635f521d37c470621d24d
nasuda <nasuda908@gmail.com>
2026-01-14 10:22:40 +0900

    add css

--- second-week/page/server.mjs
+++ second-week/page/server.mjs
@@ -5,8 +5,16 @@ console.log(createServer);
 
 const html = readFileSync("index.html");
 const js = readFileSync("main.js");
+const css = readFileSync("styles.css");
 
 const server = createServer((req, res) => {
+
+  if (req.url === "/styles.css") {
+    res.writeHead(200, { "Content-Type": "text/css; charset=utf-8" });
+    res.end(css);
+    return;
+  }
+
   if (req.url === "/main.js") {
     res.writeHead(200, { "Content-Type": "text/javascript; charset=utf-8" });
     res.end(js);
