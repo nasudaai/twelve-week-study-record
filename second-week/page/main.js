36e58efb0b014c3e0ddf85fb584cdd15baed55cb
nasuda <nasuda908@gmail.com>
2026-01-14 10:15:03 +0900

    add front js

--- second-week/page/main.js
+++ second-week/page/main.js
@@ -1,7 +1,12 @@
 console.log("loaded");
 
+let isBlack = false;
+
+
 const body = document.querySelector("body");
-body.addEventListener("click", () => {
-  body.style.backgroundColor = "green";
-});
+
+  body.addEventListener("click", () => {
+    isBlack = !isBlack;
+    body.style.backgroundColor = isBlack ? "black" : "ivory";
+  });
 console.log(body);
