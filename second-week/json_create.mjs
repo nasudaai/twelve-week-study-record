cbb8862a7ad2e215d28dbf3f3a54306fdc7b0c85
nasuda <nasuda908@gmail.com>
2026-01-16 02:26:29 +0900

    add err process

--- second-week/json_create.mjs
+++ second-week/json_create.mjs
@@ -1,6 +1,11 @@
 const args = process.argv.slice(2);
 //console.log(args);
 
+if (args.length !== 2) {
+  process.stdout.write("usage: <key> <value>\n");
+  process.exit(1);
+}
+
 const obj = {};
 const key = args[0];
 const value = args[1];
