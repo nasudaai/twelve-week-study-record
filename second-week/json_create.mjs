261803145524021e99cfc5e52c785ce687c205f6
nasuda <nasuda908@gmail.com>
2026-01-16 02:16:56 +0900

    add argv -> json

--- second-week/json_create.mjs
+++ second-week/json_create.mjs
@@ -1,4 +1,16 @@
 const args = process.argv.slice(2);
-console.log(args);
+//console.log(args);
 
-process.stdout.write(JSON.stringify(args));
+const obj = {};
+const key = args[0];
+const value = args[1];
+
+obj[key] = value;
+
+console.log(obj);
+
+
+const json = JSON.stringify(obj);
+
+process.stdout.write(JSON.stringify(args) + "\n");
+process.stdout.write(json + "\n");
