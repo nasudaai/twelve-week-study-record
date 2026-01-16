ff0268c31859d3ae93312050457b5584b5700ea3
nasuda <nasuda908@gmail.com>
2026-01-17 00:43:15 +0900

    add json

--- example/main.mjs
+++ example/main.mjs
@@ -1,8 +1,12 @@
 const str = "string";
 
+const obj = {key: "value"};
+const json = JSON.stringify(obj);
+
 function write(str) {
 
   process.stdout.write(str + "\n");
 };
 
 write(str);
+write(json);
