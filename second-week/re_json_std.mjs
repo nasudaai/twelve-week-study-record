d941ebdce804c5deef0e9311f39f89b9006c55f7
nasuda <nasuda908@gmail.com>
2026-01-21 00:31:18 +0900

    tidied it up a bit

--- second-week/re_json_std.mjs
+++ second-week/re_json_std.mjs
@@ -2,12 +2,19 @@ const args = process.argv.slice(2);
 const keyStr = "key";
 const valueStr = "value";
 
-const obj = {keyStr: valueStr};
+const obj = { [keyStr] : valueStr};
 const json = JSON.stringify(obj);
+console.log(json);
 
 
 const structure = {
   key: "value"
 };
 
+structure["key"] = args[0]
 process.stdout.write(JSON.stringify(structure) + "\n");
+
+/*
+const object = Object.create({x: 1});
+console.log(object);
+*/
