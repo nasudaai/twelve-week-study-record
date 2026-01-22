febc155df789ef5f84cdedee47a89a75ba6ad69f
nasuda <nasuda908@gmail.com>
2026-01-23 02:06:04 +0900

    add editor

--- make-something-to-use/editor/server.mjs
+++ make-something-to-use/editor/server.mjs
@@ -1,2 +1,10 @@
 import { createServer } from 'node:http';
-console.log(createServer);
+//console.log(createServer);
+
+const server = createServer((req, res) => {
+  res.end(`<h1>editor</h1> <textarea style="width: 100%; box-sizing: border-box;"></textarea>`)
+});
+
+server.listen(3000, () => {
+  console.log("listen http://localhost:3000");
+});
